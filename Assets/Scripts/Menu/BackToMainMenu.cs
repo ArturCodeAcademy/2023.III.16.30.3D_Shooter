c7d27c89ba60896951953288df0b9d793da9fac9
Artur <Artur@email.com>
2024-01-17 17:39:52 +0200

    Tank

---
+++ Assets/Scripts/Menu/BackToMainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+			SceneManager.LoadScene(0);
+		}
+    }
+}
